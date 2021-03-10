fc82906e1ac42003c8059213440d606c13bd7782
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-10 21:52:24 +0530

    Notification added

---
+++ main.py
@@ -0,0 +1,12 @@
+from plyer import notification
+
+def notifyMe(title,message):
+    notification.notify(
+        title=title,
+        message =message,
+        app_icon="F:\PythonProjects\COVID NOTIFY\icon.png",
+        timeout=10
+    )
+
+if __name__ == '__main__':
+    notifyMe("Shobhit","What is this")
