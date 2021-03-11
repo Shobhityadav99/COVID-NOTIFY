449ccb0b0db1f93119af5e4b7d8813d5c4a8aed7
Shobhit Yadav <shobhit999000@gmail.com>
2021-03-11 18:33:15 +0530

    Html fetched

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from plyer import notification
+import requests
+from bs4 import BeautifulSoup
 
 def notifyMe(title,message):
     notification.notify(
@@ -8,5 +10,11 @@ def notifyMe(title,message):
         timeout=10
     )
 
+def getData(url):
+    return requests.get(url).text
+
 if __name__ == '__main__':
     notifyMe("Shobhit","What is this")
+    myHtml= getData("https://www.mohfw.gov.in/")
+    soup = BeautifulSoup(myHtml, 'html.parser')
+    print(soup.prettify())
